d0715993001e6be1ad0d96646c9fb96fa0e33967
dburtnja <dostup.te@gmail.com>
2019-05-04 11:28:08 +0300

    test

--- market/consumers.py
+++ market/consumers.py
@@ -1,9 +1,21 @@
 import asyncio
 from channels.consumer import AsyncConsumer
+from time import sleep
 
 
 class TestConsumer(AsyncConsumer):
 
-    def websocket_connect(self, event):
+    async def websocket_connect(self, event):
         print("event:", event)
+        await self.send({
+            "type": "websocket.accept"
+        })
+        while True:
+            await asyncio.sleep(5)
+            await self.send({
+                'type': 'websocket.send',
+                'text': "test",
+            })
 
+    async def websocket_receive(self, event):
+        print("received:", event)
--- market/views.py
+++ market/views.py
@@ -8,4 +8,6 @@ import requests
 def market(request):
     proxies = {"http": "http://61.233.25.166:80"}
     result = requests.get("https://booking.uz.gov.ua/", proxies=proxies)
+    print(result)
+    print(result.reason)
     return HttpResponse(result.content)
